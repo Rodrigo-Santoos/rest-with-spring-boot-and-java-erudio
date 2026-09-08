958ff32c7cf6377d26501b716559d3a3cdc54405
Rodrigo Oliveira <rodrigoyukimide@gmail.com>
2026-09-08 18:07:53 -0300

    primeiro passo cem java com spring boot 3

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/rodrigo_santoos/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.rodrigo_santoos.model;
+
+public record Greeting(Long id, String content) {
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/rodrigo_santoos/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigo_santoos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
